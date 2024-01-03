35dba2c070613e57ce811958b04f9b3ab0a74ad9
Shahrukh Khaliq <shahrukh.khaliq@kualitatem.com>
2024-01-04 00:29:35 +0500

    xdsdz

--- Scripts/katalon sample/Script1703603655254.groovy
+++ Scripts/katalon sample/Script1703603655254.groovy
@@ -4,7 +4,6 @@ import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
 import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
 import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
 import com.kms.katalon.core.model.FailureHandling as FailureHandling
@@ -24,6 +23,8 @@ WebUI.navigateToUrl('https://kualiteedemo.kualitee.com/')
 
 WebUI.setText(findTestObject('Object Repository/inputemail_id'), 'shahrukh.khaliq@kualitatem.com')
 
+WebUI.takeAreaScreenshotAsCheckpoint('Login Dialog Origin', null)
+
 WebUI.setEncryptedText(findTestObject('Object Repository/inputpassword'), 'MV9KurHwBdXuN2kGL7auTg==')
 
 WebUI.click(findTestObject('Object Repository/input_submit-btn'))
@@ -31,6 +32,7 @@ WebUI.click(findTestObject('Object Repository/input_submit-btn'))
 WebUI.rightClick(findTestObject('Object Repository/span_1'))
 
 WebUI.click(findTestObject('Object Repository/span_1'))
+WebUI.takeAreaScreenshotAsCheckpoint('Login Dialog Origin', null)
 
 WebUI.click(findTestObject('Object Repository/span_1'))
 
@@ -41,8 +43,10 @@ WebUI.setText(findTestObject('Object Repository/input_tc_name'), 'xsdsd')
 WebUI.setText(findTestObject('Object Repository/textarea_summary'), 'sdfsdfsdf')
 
 WebUI.click(findTestObject('Object Repository/button_Save'))
+WebUI.takeElementScreenshot('Object Repository/button_Save')('Login Dialog Origin', null)
 
 WebUI.click(findTestObject('Object Repository/button_Save'))
+
 WebUI.takeScreenshot('Defect_Created_Successfully')
 
 WebUI.closeBrowser()
